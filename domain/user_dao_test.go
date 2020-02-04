1d13d009ddda57fac8cf684c62d1d9cbeb382f49
Naufal Ziyad <naufal.ziyad@detik.com>
2020-02-04 15:59:55 +0700

    [add]unit test

---
+++ domain/user_dao_test.go
@@ -0,0 +1,29 @@
+package domain
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserNoUserFound(t *testing.T) {
+	user, err := GetUser(0)
+	assert.Nil(t, user, "we are not expectiong a user with id 0")
+	assert.NotNil(t, err)
+
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 not found", err.Message)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+
+	assert.EqualValues(t, 123, user.ID)
+	assert.EqualValues(t, "Naufal", user.FirstName)
+	assert.EqualValues(t, "Ziyad", user.LastName)
+	assert.EqualValues(t, "naufal.ziyad@detik.com", user.Email)
+}
